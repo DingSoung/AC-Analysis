b91e84ffbc909bec6ef6c129ea4379839b96130a
Ding Soung <DingSoung@gmail.com>
2014-06-20 16:23:08 +0800

    crc
    add crc32

---
+++ crc.h
@@ -0,0 +1,8 @@
+﻿#ifndef ___CRC_H___
+#define ___CRC_H___
+
+void crc32Init(unsigned long *pCrc32);
+void crc32Update(unsigned long *pCrc32, unsigned char *pData, unsigned long uSize);
+void crc32Finish(unsigned long *pCrc32);
+
+#endif /* ___CRC_H___ */
